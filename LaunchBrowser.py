6724bb6a72e852d6e9b4ac3d675df2d1d04cca06
nazeertsm <59368254+nazeertsm@users.noreply.github.com>
2021-07-15 14:15:49 +0530

    Update LaunchBrowser.py

--- LaunchBrowser.py
+++ LaunchBrowser.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 
-driver = webdriver.Chrome("C:\\N4\\VSCODE\\SeleniumpythonCode\\chromedriver.exe")
+driver = webdriver.Chrome("C:\\SP\\VSCODE\\SeleniumpythonCode\\chromedriver.exe")
 driver.maximize_window()
 driver.get("https://www.google.com/?safe=active&ssui=on")
 print(driver.title)
